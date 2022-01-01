2a6c7d7810c9917c7eb179d83bdccf32a7dd9e81
BJ Abichuela <bjabichuela@gmail.com>
2022-01-01 13:45:00 +0800

    Updated footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <Container className="footer">
                 <Row>
                     <Col className='text-center' py-3='true' style={{ fontSize: 15 }}>
-                        Copyright &copy; 2021 | Built by John Abichuela
+                        Copyright &copy; 2022 | Built by John Abichuela
                     </Col>
                 </Row>
             </Container>
